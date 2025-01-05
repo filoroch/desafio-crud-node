57191aec73ddff7b6ebfc131dc1ee47cd15d405c
filoroch <dev.filoroch@gmail.com>
2025-01-05 12:40:02 -0300

    Add connection.js
    Modulo de conexão com o banco de dados. No momento, o banco é um JSON
    
    Signed-off-by: filoroch <dev.filoroch@gmail.com>

---
+++ database/connection.js
@@ -0,0 +1,18 @@
+const {Low, JSONFile} = require('lowdb');
+const path = require('path');
+
+// Caminho para o JSON que ta armazenando os dados
+const file = path.join(__dirname, 'database.json');
+const adapter = new JSONFile(file);
+
+const db = new Low(adapter);
+
+const initializeDB = async () => {
+    await db.read(); // Carrega os dados do arquivo;
+    db.data = {usuarios : [], funcionarios: [], departamentos: [], cargos: [] }; // Estrutura do JSON
+    await db.write(); // Salve os dados no arquivo
+};
+
+initializeDB();
+
+module.exports = db;
